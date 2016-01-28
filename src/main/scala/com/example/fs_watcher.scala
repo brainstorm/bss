dbdde46559f749d9cbd544a8eedbd97e696b8c67
Roman Valls Guimera <brainstorm@nopcode.org>
2016-01-28 19:07:08 +0100

    With Scala RxMonitor it takes ~10 seconds on OSX for the ENTRY_MODIFY to be detected. Switching to pure Akka instead to avoid potential buffering

--- src/main/scala/com/example/fs_watcher.scala
+++ src/main/scala/com/example/fs_watcher.scala
@@ -1,7 +1,7 @@
 package com.example
 
 import akka.actor.ActorSystem
-import com.beachape.filemanagement.RxMonitor
+import com.beachape.filemanagement.{MonitorActor, RxMonitor}
 
 import java.nio.file.Paths
 import java.nio.file.StandardWatchEventKinds._
@@ -10,6 +10,7 @@ object sch_fs_watcher {
   def main(args: Array[String]): Unit = {
 
     implicit val system = ActorSystem("actorSystem")
+    val fileMonitorActor = system.actorOf(MonitorActor(concurrency = 2))
 
     val monitor = RxMonitor()
     val observable = monitor.observable
@@ -20,22 +21,12 @@ object sch_fs_watcher {
       onCompleted = { () => println("File sent to common sequencing bus") }
     )
 
-    val flowcells = Paths get "/Users/romanvg/tiny-test-data/flowcell"
+    //val flowcells = Paths get "/Users/romanvg/tiny-test-data/flowcell"
 
-    monitor.registerPath(ENTRY_CREATE, flowcells)
+    val flowcells = Paths get "/Users/romanvg/Desktop/flowcell"
 
-    //Thread.sleep(100)
+    monitor.registerPath(ENTRY_MODIFY, flowcells)
 
-    //modify a monitored file
-    //val writer = new BufferedWriter(new FileWriter(flowcells.toFile))
-    //writer.write("Theres text in here wee!!")
-    //writer.close
-
-    // #=> Something was modified in a file mufufu: /Users/lloyd/Desktop/test
-
-    // stop monitoring
-    monitor.stop()
-
-    // #=> Monitor has been shut down
+    //monitor.stop()
   }
 }
